902ece4a7c117ae306dbbe0e790adfdbb622824e
ills5771 <ilyayelizarov1@gmail.com>
2019-03-25 20:56:43 -0700

    added put route to update zoo information

--- index.js
+++ index.js
@@ -57,6 +57,23 @@ server.post("/api/zoos", async (req, res) => {
   }
 });
 
+server.put("/api/zoos/:id", async (req, res) => {
+  try {
+    const count = await db("zoos")
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const zoo = await db("zoos")
+        .where({ id: req.params.id })
+        .first();
+      res.status(200).json(zoo);
+    } else {
+      res.status(404).json({ errorMessage: "Zoo not found" });
+    }
+  } catch (error) {}
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
